663d38d00d09da9701cc98c084eda3564910431e
keerthanlalith <keerthanlalith@gmail.com>
2021-04-07 05:29:59 +0200

    commenting

--- Simple.py
+++ Simple.py
@@ -6,38 +6,31 @@ from tensorflow.keras.optimizers import SGD , Adam, RMSprop
 from tensorflow.keras.layers import Concatenate, Dense, LSTM, Input, concatenate
 from tensorflow.keras.layers import LeakyReLU 
 from matplotlib import pyplot
-
-#from keras.utils.vis_utils import plot_model
-
-
-
 import pickle 
 import copy
 
 
-# This is our input image
+# Input state
 curr_state = keras.Input(shape=(4,))
 
-# "encoded" is the encoded representation of the input
+# 2layer neural network to predict the next state
 encoded = Dense(256)(curr_state)
 encoded = LeakyReLU(alpha=0.2)(encoded)
 encoded = Dense(256)(encoded)
 encoded = LeakyReLU(alpha=0.2)(encoded)
 n_state = layers.Dense(4)(encoded)
 
-# This model maps an input to its encoded representation
+# This model maps an input to its next state
 AE = keras.Model(inputs=curr_state, outputs=n_state)
 
 #print(AE.summary())
 tf.keras.utils.plot_model(AE, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-
 
 lr_schedule = keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=5e-4,
     decay_steps=1000,
     decay_rate=0.9)
 #opt = keras.optimizers.Adam(learning_rate=lr_schedule)
-
 #opt = SGD(lr=0.005, momentum=0.9)
 #opt = Adam(lr=0.0005)
 opt = tf.keras.optimizers.RMSprop(learning_rate=0.00015)
@@ -45,7 +38,6 @@ AE.compile(loss='mean_squared_error', optimizer=opt, metrics=['mse'])
 #AE.compile(loss='mean_squared_logarithmic_error', optimizer=opt, metrics=['mse'])
 
 # Import Data
-
 filename = 'Data/NState.npy'
 ns = pickle.load(open(filename, 'rb'))
 filename = 'Data/Action.npy'
@@ -69,13 +61,12 @@ pause = 0
 while pause == 1:
     pause = 1
 
-
-#history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=100, verbose=0)
-
+# training
 history = AE.fit(x=s, y=ns,
                 epochs=100,
                 batch_size=32,
                 shuffle=True,
+                verbose=0,
                 validation_data=(test_s, test_ns))
 '''
 history = AE.fit(x=s, y=ns,
@@ -96,6 +87,7 @@ while pause == 1:
 _, train_mse = AE.evaluate(s, ns, verbose=0)
 _, test_mse = AE.evaluate(test_s, test_ns, verbose=0)
 print('Train: %.6f, Test: %.6f' % (train_mse, test_mse))
+
 # plot loss during training
 pyplot.subplot(211)
 pyplot.title('Loss')
@@ -115,15 +107,21 @@ while pause == 1:
     pause = 1
 
 
-# Encode and decode some digits
+# Predict next state 
 # Note that we take them from the *test* set
 pred_ns = AE.predict(test_s)
+d_err = np.abs(test_ns - pred_ns)
+#pred_ns = denormalise (pred_ns)
+d_err_1 = np.abs(test_ns - pred_ns)
+p_err =np.mean(d_err*d_err,axis=0)
+p_err2 =np.mean(d_err_1*d_err_1,axis=0)
+    
+print(p_err,p_err2)
 
 n=len(test_s)
 n = int(input('Enter your number of test data to predict:'))
 
 print ("AE diff output, Normalised Test diff,  Difference")
-d_err = np.abs(test_ns - pred_ns)
     
 for i in range(n):
     for j in range(4):
@@ -150,8 +148,5 @@ for i in range(n):
     print(pred_ns[i],test_ns[i],d_err_1[i])
 print("------")
 
-p_err =np.mean(d_err*d_err,axis=0)
-p_err2 =np.mean(d_err_1*d_err_1,axis=0)
-    
-print(p_err,p_err2)
+
     
